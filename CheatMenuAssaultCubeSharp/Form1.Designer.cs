23655832563dcdf64a3734965731cacb30363a5b
juduj <153448858+juduj@users.noreply.github.com>
2023-12-18 19:51:46 -0500

    Add files via upload

---
+++ CheatMenuAssaultCubeSharp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace CheatMenuAssaultCubeSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            // Close the process handle if it has been opened
+            if (procHandle != IntPtr.Zero)
+            {
+                CloseHandle(procHandle);
+                procHandle = IntPtr.Zero; // Reset the handle after closing
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(183, 29);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Unlimited Ammo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Segoe MDL2 Assets", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(129, 9);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(66, 19);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "Status: ";
+            this.StatusLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Attach";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 309);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CheatMenuAssaultCubeSharp/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace CheatMenuAssaultCubeSharp
+{
+    public partial class Form1 : Form
+    {
+        bool unlimitedAmmo = false;
+
+        IntPtr baseAddress;
+        IntPtr procHandle;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+        uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress,
+        uint dwSize, uint dwFreeType);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress,
+        int dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle,
+        uint dwProcessId);
+
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,
+          byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        // Define constants for allocation type and memory protection
+        [Flags]
+        public enum AllocationType : uint
+        {
+            MEM_COMMIT = 0x1000,
+            MEM_RESERVE = 0x2000,
+            MEM_RESET = 0x80000,
+            MEM_RESET_UNDO = 0x1000000
+        }
+
+        [Flags]
+        public enum MemoryProtection : uint
+        {
+            PAGE_EXECUTE_READWRITE = 0x40
+        }
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        bool AttachGame()
+        {
+            Process[] processes = Process.GetProcessesByName("ac_client");
+
+            if (processes.Length > 0)
+            {
+                // Access the first process (you might need to handle multiple processes with the same name differently)
+                Process targetProcess = processes[0];
+               
+                // Store the Process ID (PID) in a variable
+                int processId = targetProcess.Id;
+                
+                // Get the base address of the main module (executable) of the process
+                baseAddress = targetProcess.MainModule.BaseAddress;
+
+                StatusLabel.Text = "Game found!";
+
+                // Now you have the Process ID (processId) and the base address (baseAddress) to use as needed
+                procHandle = targetProcess.Handle;
+
+                return true;
+            }
+            else
+            {
+                StatusLabel.Text = "Game not found!";
+
+                return false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                unlimitedAmmo = true;
+            }
+            else if(!checkBox1.Checked)
+            {
+                unlimitedAmmo = false;
+            }
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //so our game doesnt edit address values if that address is not initalized.
+            if (AttachGame())
+            {
+                if(unlimitedAmmo)
+                {
+                    int numOfBytes;
+                    uint oldP;
+                    byte[] hackAmmo = { 0x90, 0x90 };
+
+                    VirtualProtectEx(procHandle, (IntPtr)0x004637E9, 2, 0x40, out oldP);
+
+                    unsafe
+                    {
+                        WriteProcessMemory(procHandle, (IntPtr)0x004637E9, hackAmmo, (uint)hackAmmo.Length, out numOfBytes);
+                    }
+                }
+            }
+        }
+      
+    }
+}
